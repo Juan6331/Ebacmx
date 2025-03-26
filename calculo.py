6a7fc79e7dd7ed0899a76c02deee6cad1294d178
Juan6331 <108277159+Juan6331@users.noreply.github.com>
2025-03-26 07:44:22 -0600

    se agrego la variable e

--- calculo.py
+++ calculo.py
@@ -11,3 +11,7 @@ print("El resultado del cálculo es:", resultado)
 #cambio a factor 200
 d= resultado*200
 print('resultado final',d)
+e=1.1
+f=d*e
+print('resultado final con factor de 1.1',f)
+#se añadio un factor de 1.1
